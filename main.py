90f9153ee490e2296235a50909a007b394fc7fae
Maxim <mivanov222@gmail.com>
2023-03-17 12:49:43 +0300

    Add some changes

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ c = int(n[2])
 print('Sum are: ', a + b + c)
 
 
+###################################### Another variant###############################
+
+
+n = input("Enter your number: ")
+n = int(n)
+ 
+d1 = n % 10
+d2 = n % 100 // 10
+d3 = n // 100
+ 
+print("Sum are:", d1 + d2 + d3)
